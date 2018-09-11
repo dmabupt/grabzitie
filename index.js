45577d62d1c333e1cb475bb2138da5c82f60e1f1
mengxumx <mengxumx@cn.ibm.com>
2018-09-11 08:17:43 +0800

    add totf.py for tfrecords

--- index.js
+++ index.js
@@ -6,48 +6,49 @@ const tagDivG = /<div class="imgInfo fl">[\s\S]+?<\/div>/g;
 const tagImgG = /<img src="([\s\S]+?)" width/;
 const tagLabelG = /imgAuthor">([\s\S]+?)<\/p>/;
 
-const zitis = ["k", "x", "c", "l"];
-const text = ["的", "一", "不", "是", "了", "人", "在", "有", "我", "他", "这", "为", "之", "来", "大", "以", "个", "中", "上", "们", "到", "说", "国", "和", "地", "也", "子", "时", "道", "出", "而", "要", "于", "就", "下", "得", "可", "你", "年", "生", "自", "会", "那", "后", "能", "对", "着", "事", "其", "里", "所", "去", "行", "过", "家", "十", "用", "发", "天", "如", "然", "作", "方", "成", "者", "多", "日", "都", "三", "小", "军", "二", "无", "同", "么", "经", "法", "当", "起", "与", "好", "看", "学", "进", "种", "将", "还", "分", "此", "心", "前", "面", "又", "定", "见", "只", "主", "没", "公", "从"];
+const ziti = ['k', 'x', 'c'];
+const text = ['的', '一', '不', '是', '了', '人', '在', '有', '我', '他', '这', '为', '之', '来', '大', '以', '个', '中', '上', '们', '到', '说', '国', '和', '地', '也', '子', '时', '道', '出', '而', '要', '于', '就', '下', '得', '可', '你', '年', '生', '自', '会', '那', '后', '能', '对', '着', '事', '其', '里', '所', '去', '行', '过', '家', '十', '用', '发', '天', '如', '然', '作', '方', '成', '者', '多', '日', '都', '三', '小', '军', '二', '无', '同', '么', '经', '法', '当', '起', '与', '好', '看', '学', '进', '种', '将'];
 
 async function download(name, label, cb) {
   if(fs.existsSync(label) == false)
     fs.mkdirSync(label);
-  var dest = label + "/" + name.replace(/\//g,'_');
+  var dest = label + '/' + name.replace(/\//g,'_');
   console.log(dest);
   if(fs.existsSync(dest) == false) {
     var file = fs.createWriteStream(dest);
     var url = 'http://www.saishufa.com/' + name;
     console.log(url);
-    await http.get(url, (response) => {
+    http.get(url, (response) => {
       response.pipe(file);
       file.on('finish', () => { file.close(); });
-    }).on('error', (err) => { fs.unlink(dest); });
+    }).on('error', (err) => {  });
   }
 };
 
-async function run(ziti, zi) {
+async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const url = 'http://www.saishufa.com/search.html?ziti=' + ziti + '&word=' + encodeURI(zi);
-  console.log(url);
-  await page.goto(url);
-  const bodyHandle = await page.$('body');
-  const bodyHTML = await page.evaluate(body => body.innerHTML, bodyHandle);
-  // console.log(bodyHTML);
-
-  const divs = bodyHTML.match(tagDivG);
-  if(divs && divs.length > 0) {
-    for(i = 0; i < divs.length - 1; i++) {
-      const img = divs[i].match(tagImgG);
-      const label = divs[i].match(tagLabelG);
-      if(img && img.length > 1 && label && label.length > 1 && label[1].length < 4) {
-        await download(img[1], label[1]);
+  for(k in ziti) {
+    for(j in text) {
+      const url = 'http://www.saishufa.com/search.html?ziti=' + ziti[k] + '&word=' + encodeURI(text[j]);
+      console.log(url);
+      await page.goto(url, {timeout: 0});
+      const bodyHandle = await page.$('body');
+      const bodyHTML = await page.evaluate(body => body.innerHTML, bodyHandle);
+      // console.log(bodyHTML);
+      const divs = bodyHTML.match(tagDivG);
+      if(divs && divs.length > 0) {
+        for(var i = 0; i < divs.length - 1; i++) {
+          const img = divs[i].match(tagImgG);
+          const label = divs[i].match(tagLabelG);
+          if(img && img.length > 1 && label && label.length > 1 && label[1].length < 4) {
+            await download(img[1], label[1]);
+          }
+        }
       }
     }
   }
   await browser.close();
 };
 
-for(ziti in zitis)
-  for(zi in text)
-    run(ziti, zi);
+run();
---
+++ totf.py
@@ -0,0 +1,54 @@
+import math
+import os
+import sys
+import random
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+labels = { "csl":0, "htj":1, "mf":2, "oyx":3, "sgt":4, "wxz":5, "wxaz":6, "xbq":7, "yzq":8, "zsj":9 }
+
+def _int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+  
+def _get_filenames_and_classes(dataset_root):
+  with tf.python_io.TFRecordWriter('output.tfrecords') as record_writer:
+    for dirname in os.listdir(dataset_root):
+      dirpath = os.path.join(dataset_root, dirname)  # zitie/yzq
+      print(dirpath)
+      if os.path.isdir(dirpath):
+        label = labels[dirname] # label = yzq
+        for filename in os.listdir(dirpath):
+          filepath = os.path.join(dirpath, filename)  # filepath = zitie/yzq/0001.png
+          print(filepath)
+          image_raw_data = tf.gfile.FastGFile(filepath, 'rb').read()
+          with tf.Session() as sess:
+            if(filename.startswith('png')):
+              img_data = tf.image.decode_png(image_raw_data, 1)
+              img_data.set_shape([538, 528, 1])
+            else:
+              img_data = tf.image.decode_png(image_raw_data, 4)
+              img_data.set_shape([538, 528, 4])
+              img_data = tf.slice(img_data, [0, 0, 3], [538, 528, 1])
+            img_data = tf.image.resize_images(img_data, [128, 128])
+            #img_data = tf.image.convert_image_dtype(img_data, dtype = tf.float32)
+            img_data = tf.squeeze(img_data)
+            img_data = tf.where(img_data>10, 255*tf.ones_like(img_data), tf.zeros_like(img_data))
+            # plt.figure(1)
+            # plt.imshow(img_data.eval())
+            # plt.title(label)
+            # plt.show()
+            example = tf.train.Example(features=tf.train.Features(feature={
+              'image_raw': _bytes_feature(img_data.eval().tobytes()),
+              'label': _int64_feature(label)
+            }))
+            record_writer.write(example.SerializeToString())
+
+def main():
+    _get_filenames_and_classes('zitie')
+
+if __name__ == "__main__":
+    main()
