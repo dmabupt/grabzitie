e279e8a7b81c23cf18b5f9cd14f5a38a2ccf37e7
dmabupt <dmabupt@gmail.com>
2018-09-13 07:21:52 +0800

    update index.js

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const tagImgG = /<img src="([\s\S]+?)" width/;
 const tagLabelG = /imgAuthor">([\s\S]+?)<\/p>/;
 
 const ziti = ['k', 'x', 'c'];
-const text = ['的', '一', '不', '是', '了', '人', '在', '有', '我', '他', '这', '为', '之', '来', '大', '以', '个', '中', '上', '们', '到', '说', '国', '和', '地', '也', '子', '时', '道', '出', '而', '要', '于', '就', '下', '得', '可', '你', '年', '生', '自', '会', '那', '后', '能', '对', '着', '事', '其', '里', '所', '去', '行', '过', '家', '十', '用', '发', '天', '如', '然', '作', '方', '成', '者', '多', '日', '都', '三', '小', '军', '二', '无', '同', '么', '经', '法', '当', '起', '与', '好', '看', '学', '进', '种', '将'];
+const text = ['的', '一', '国', '在', '人', '了', '有', '中', '是', '年', '和', '大', '业', '不', '为', '发', '会', '工', '经', '上', '地', '市', '要', '个', '产', '这', '出', '行', '作', '生', '家', '以', '成', '到', '日', '民', '来', '我', '部', '对', '进', '多', '全', '建', '他', '公', '开', '们', '场', '展', '时', '理', '新', '方', '主', '企', '资', '实', '学', '报', '制', '政', '济', '用', '同', '于', '法', '高', '长', '现', '本', '月', '定', '化', '加', '动', '合', '品', '重', '关', '机', '分', '力', '自', '外', '者', '区', '能', '设', '后', '就', '等', '体', '下', '万', '元', '社', '过', '前', '面'];
 
-async function download(name, label, cb) {
-  if(fs.existsSync(label) == false)
-    fs.mkdirSync(label);
-  var dest = label + '/' + name.replace(/\//g,'_');
+function sleep(ms=3000){
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function download(name, ziti, label) {
+  if(fs.existsSync(ziti + '_' + label) == false)
+    fs.mkdirSync(ziti + '_' + label);
+  var dest = ziti + '_' + label + '/' + name.replace(/\//g,'_');
   console.log(dest);
   if(fs.existsSync(dest) == false) {
     var file = fs.createWriteStream(dest);
@@ -26,26 +30,53 @@ async function download(name, label, cb) {
 };
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage();
+  
+  page.setViewport({
+      width: 1200,
+      height: 800
+  })
+
   for(k in ziti) {
     for(j in text) {
       const url = 'http://www.saishufa.com/search.html?ziti=' + ziti[k] + '&word=' + encodeURI(text[j]);
       console.log(url);
-      await page.goto(url, {timeout: 0});
-      const bodyHandle = await page.$('body');
-      const bodyHTML = await page.evaluate(body => body.innerHTML, bodyHandle);
-      // console.log(bodyHTML);
-      const divs = bodyHTML.match(tagDivG);
-      if(divs && divs.length > 0) {
-        for(var i = 0; i < divs.length - 1; i++) {
-          const img = divs[i].match(tagImgG);
-          const label = divs[i].match(tagLabelG);
-          if(img && img.length > 1 && label && label.length > 1 && label[1].length < 4) {
-            await download(img[1], label[1]);
-          }
-        }
-      }
+      await page.goto(url, {waitUntil: 'load'});
+      var scrollTimer = page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 600;
+          var timer = setInterval(() => {
+            window.scrollBy(0, distance)
+            totalHeight += distance;
+            if(totalHeight >= document.body.scrollHeight){
+              clearInterval(timer);
+              resolve();
+            }
+          }, 500);
+        })
+      })
+
+      var crawler = scrollTimer.then(async () => {
+        var urls = await page.evaluate(() => {
+          var links = [...document.querySelectorAll('div .imgInfo .fl')];
+          return links.map(el => {
+            const imgname = el.innerText.match(tagImgG);
+            const labelname = el.innerText.match(tagLabelG);
+            return {img: imgname, label: labelname};
+          });
+        })
+        await page.close()
+        return Promise.resolve(urls)
+      }).catch((e) => {
+        console.log(e);
+      });
+
+      crawler.then(urls => {
+        console.log(urls.img + k + urls.label);
+        // await download(urls.img, k, urls.label);
+      });
     }
   }
   await browser.close();
